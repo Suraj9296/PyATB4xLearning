456387e367aeb3a2e315f7daa54f93ee739af4f0
Suraj <suriprasadj@hotmail.com>
2024-08-14 11:40:30 +0530

    Added New folder

---
+++ src/ex_08082024/LAB003.py
@@ -0,0 +1,4 @@
+print("Hello Pramod, Welcome to the learning Python.")
+print(2+2)
+print(2-2)
+print(2-2)
---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,5 @@
+# This is  Comment - Which does not run
+print("Hello Pramod, Welcome to the learning Python.")
+print(2+2)
+print(2-2)
+print(2*2)
